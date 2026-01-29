d47483436b0fec43c3c7356eb3dbbf49c363b7b9
Nikita Astafyev <nikita.astafyev@gmail.com>
2026-01-29 16:33:51 +0300

    fix: gcc unknown pragmas

--- tests/main.c
+++ tests/main.c
@@ -1,7 +1,13 @@
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-but-set-variable"
+#if defined(__clang__)
+#  pragma clang diagnostic push
+#  pragma clang diagnostic ignored "-Wunused-but-set-variable"
+#endif
+
 #include "tau/tau.h"
-#pragma clang diagnostic pop
+
+#if defined(__clang__)
+#  pragma clang diagnostic pop
+#endif
 
 
 TAU_MAIN()
