f428d930f0ec30b9f885bfa3be24e2bc78e2f8e8
Nikita Astafyev <nikita.astafyev@gmail.com>
2026-03-12 19:24:25 +0300

    feat: implemented non rehashable hashmap

--- platform/rars/memory.c
+++ platform/rars/memory.c
@@ -25,12 +25,16 @@ void jackc_free(void* ptr) {
     (void) ptr; // We don't need to do anything here as rars doesn't support freeing memory
 }
 
-// GCC can add implicit calls to memcpy. Not meant to be used directly.
-__attribute__((weak)) void *memcpy(void* dest, const void* src, size_t n) {
+void* jackc_memcpy(void* dest, const void* src, size_t n) {
     char* d = dest;
     const char* s = src;
     while (n--) {
         *d++ = *s++;
     }
     return dest;
+}
+
+// GCC can add implicit calls to memcpy. Not meant to be used directly.
+__attribute__((weak)) void* memcpy(void* dest, const void* src, size_t n) {
+    return jackc_memcpy(dest, src, n);
 }
