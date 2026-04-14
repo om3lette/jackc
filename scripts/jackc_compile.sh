45bab1bc0d764ca584a5008dd9ede0b45d7c1975
Nikita Astafyev <nikita.astafyev@gmail.com>
2026-04-15 01:16:17 +0300

    fix: rars build

--- scripts/jackc_compile.sh
+++ scripts/jackc_compile.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 <path_to_rars> <jack_sources_dir> <output_dir>"
+    echo "Usage: $0 <path_to_rars> <jack_sources_dir> <output_dir> <stdlib_dir>"
     exit 1
 fi
 
 rars_path="$1"
 jack_sources_dir="$2"
 output_dir="$3"
+stdlib_dir="$4"
 
 jack_sources_file="jack-sources.txt"
 jack_vm_sources_file="jack-vm-sources.txt"
 
 find $jack_sources_dir -name "*.jack" -exec realpath {} \; > $jack_sources_file
-java -jar $rars_path "jackc_frontend.s" pa $jack_sources_file $output_dir
+java -jar $rars_path "jackc_frontend.s" pa -s $jack_sources_file -o $output_dir -std
 
-find $base_dir -name "*.vm" -exec realpath {} \; > $jack_vm_sources_file
-java -jar $rars_path "jackc_backend.s" pa $jack_vm_sources_file $output_dir
+find $output_dir -name "*.vm" -exec realpath {} \; > $jack_vm_sources_file
+java -jar $rars_path "jackc_backend.s" pa -s $jack_vm_sources_file -o $output_dir -std $stdlib_dir
